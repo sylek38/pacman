c6d2cbb09922321fe893b3879139cefc4a6a75ff
Sylek <sylek38@gmail.com>
2020-09-26 19:26:25 +0200

    added: food components

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Pacman from '../Pacman';
 import Ghost from '../Ghost';
+import Food from '../Food';
 import './style.css';
 
 class Board extends Component {
     render() {
         return (
           <div className="board">
-              {/* <Food /> */}
+              <Food position={{top: 100, left: 100}} />
               <Pacman />
               {/* <Ghost color="yellow" />
               <Ghost color="red" />
